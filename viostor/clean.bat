099871d401fc9294c7087618c4babe332f3cad23
Ladi Prosek <lprosek@redhat.com>
2017-08-02 12:30:49 +0200

    Revert "viostor: Remove old-style build files"
    This reverts commit 1ccfe3c1ad8c3114818d8d2af2f0dba65965c5b4.

--- viostor/clean.bat
+++ viostor/clean.bat
@@ -4,8 +4,12 @@ rmdir /S /Q .\Install
 
 for /d %%x in (objfre_*) do rmdir /S /Q %%x
 for /d %%x in (objchk_*) do rmdir /S /Q %%x
-rmdir /S /Q .\sdv
-rmdir /S /Q .\sdv.temp
+rmdir /S /Q Release\x86
+rmdir /S /Q Release\amd64
+rmdir /S /Q Release
+rmdir /S /Q Debug\x86
+rmdir /S /Q Debug\amd64
+rmdir /S /Q Debug
 
 del /F *.log *.wrn *.err *.sdf
 del viostor-2012.h
